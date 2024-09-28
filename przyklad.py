61f17ed17019e3f398d7e8848e974d48d8035de2
30470 <dpyrka@edu.cdv.pl>
2024-09-28 12:28:25 +0200

    kolejne

--- przyklad.py
+++ przyklad.py
@@ -1,14 +1,14 @@
-print("Welcome")
-# program hello world
-# komentarz
-# dalszy
-# komentarz
+# print("Welcome")
+# # program hello world
+# # komentarz
+# # dalszy
+# # komentarz
 
-print("""
-hello
-world
-program
-""")
+# print("""
+# hello
+# world
+# program
+# """)
 
 # nowy komentarz
 
@@ -24,5 +24,7 @@ j = -3
 r = 3.5
 
 print(f"Zmienna i ma wartosc: {i}.")
+print(f"Zmienna i w systemie szesnastkowym ma wartosc: {i:x}.")
+print(f"Zmienna i w systemie binarnym ma wartosc: {i:b}.")
 print(f"Zmienna j ma wartosc: {j}.")
-print(f"Zmienna r ma wartosc: {r}.")
+print(f"Zmienna r ma wartosc: {r:2.3f}.")
