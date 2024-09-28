1788a1f1041e79571ed0d0acba055fc730237476
30470 <dpyrka@edu.cdv.pl>
2024-09-28 12:46:43 +0200

    dzialania matematyczne

--- przyklad.py
+++ przyklad.py
@@ -1,3 +1,4 @@
+import math
 # print("Welcome")
 # # program hello world
 # # komentarz
@@ -22,9 +23,17 @@ print('\"siema \'')
 i = 10
 j = -3
 r = 3.5
+y = 9//2 #dzielenie calkowitoliczbowe
 
 print(f"Zmienna i ma wartosc: {i}.")
 print(f"Zmienna i w systemie szesnastkowym ma wartosc: {i:x}.")
 print(f"Zmienna i w systemie binarnym ma wartosc: {i:b}.")
 print(f"Zmienna j ma wartosc: {j}.")
-print(f"Zmienna r ma wartosc: {r:2.3f}.")
+print(f"Zmienna r ma wartosc: {r:2.3f}.") #wymusza wyswietlanie okreslonej ilosci cyfr przed/po przecinku
+print(f"Zmienna y ma wartosc: {y}")
+
+#pole kola
+r = 5
+pole = math.pi * r * r
+#print(pole)
+print("%.2f" %pole)
